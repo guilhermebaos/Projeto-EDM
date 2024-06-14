251094a2148d1fa05ac48af379413c714adf79cc
Guilherme Botelho <guilhermebaos@gmail.com>
2024-06-14 22:41:13 +0100

    Programas Coelho!

---
+++ games.py
@@ -0,0 +1,342 @@
+# Escreve o teu código aqui :-)
+
+from machine import Pin, PWM
+from time import sleep
+from random import choice
+
+
+def morse(pinr=25, ping=21, pinb=22):
+    """
+    Decifrar Código-Morse: Cria uma mensagem em código-morse (mostrada no LED RGB),
+                        que o jogador deve decifrar.
+    O LED RGB deve estar em lógica negativa.
+    --- Parâmetros ---
+    - pinr: pin ao qual está ligado a perna RED
+    - ping: pin ao qual está ligado a perna GREEN
+    - pinb: pin ao qual está ligado a perna BLUE
+    - num_words:                         #???????????????????????????????????
+    - difficulty: (nº de letras em cada) #???????????????????????????????????
+    """
+
+    # definir os LEDs
+    red = Pin(pinr, Pin.OUT)
+    red.value(1)
+    green = Pin(ping, Pin.OUT)
+    green.value(1)
+    blue = Pin(pinb, Pin.OUT)
+    blue.value(1)
+
+    # definir as sequências em código-morse dos vários caracteres
+    morse = {
+        "A": ".--",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+        "0": "-----",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+    }
+
+    # gerar a sequência de palavras do jogo
+    word_sequence = ["SOS", "OK"]
+    # usar um gerador de palavras aleatórias??????????????????????????????????????
+    # ou simplesmente sequências de números em vez de palavras????????????????????
+
+    # analisar cada palavra
+    for word in word_sequence:
+
+        # analisar cada caracter na palavra
+        for letter in word:
+
+            # associar o código do caracter
+            code = morse[letter]
+            print(letter)
+            print(code)
+
+            # mostrar no LED cada símbolo do código
+            for sym in code:
+                print(sym)
+
+                blue.value(0)
+
+                if sym == ".":
+                    sleep(0.2)
+                    blue.value(1)
+                elif sym == "-":
+                    sleep(0.6)
+                    blue.value(1)
+                sleep(0.4)
+
+            sleep(1)
+
+        sleep(3)
+
+        # resposta do jogador
+
+        # mostrar se está correta ou não
+        answer = choice([0, 1])
+
+        # avaliar a resposta do jogador
+        if answer:
+            for _ in range(6):
+                green.value(not green.value())
+                sleep(1 / 7)
+        else:
+            for _ in range(6):
+                red.value(not red.value())
+                sleep(1 / 7)
+            # condição de repetir a palavra por ter errado?????????????????????
+
+        sleep(3)
+
+    return morse
+
+
+def memory(
+        pinr=25,
+        ping=21,
+        pinb=22,
+        butr=10,
+        butg=14,
+        butb=13,
+        butres=9,
+        butsub=12,
+        num_levels=10,
+        difficulty=1,
+        ):
+    """
+    Jogo da Memória: Cria uma sequência de cores aleatória (mostrada no LED RGB),
+                    que o jogador deve repetir de seguida.
+    O LED RGB deve estar em lógica negativa.
+    --- Parâmetros ---
+    - pinr: pin ao qual está ligado a perna RED
+    - ping: pin ao qual está ligado a perna GREEN
+    - pinb: pin ao qual está ligado a perna BLUE
+    - butr: pin ao qual está ligado o botão correspondente ao RED
+    - butg: pin ao qual está ligado o botão correspondente ao GREEN
+    - butb: pin ao qual está ligado o botão correspondente ao BLUE
+    - butres: pin ao qual está ligado o botão que dá reset na resposta
+    - butsub: pin ao qual está ligado o botão que submete a resposta
+    - num_levels: número de níveis do jogo (corresponde ao nº de sequências criadas.
+                A sequência seguinte tem +1 cor que a anterior)
+    - difficulty: nível de dificuldade (associada ao tempo de pausa entre cores).
+                Pode ser 1, 2 ou 3.
+    """
+
+    # escolher a dificuldade
+    pauses_list = [1.5, 1, 0.5]
+    pause = pauses_list[difficulty - 1]
+
+    # definir os LEDs
+    red = Pin(pinr, Pin.OUT)
+    red.value(1)
+    green = Pin(ping, Pin.OUT)
+    green.value(1)
+    blue = Pin(pinb, Pin.OUT)
+    blue.value(1)
+
+    # definir os botões
+    butred = Pin(butr, Pin.IN, Pin.PULL_UP)
+    butgreen = Pin(butg, Pin.IN, Pin.PULL_UP)
+    butblue = Pin(butb, Pin.IN, Pin.PULL_UP)
+    butreset = Pin(butres, Pin.IN, Pin.PULL_UP)
+    butsubmit = Pin(butsub, Pin.IN, Pin.PULL_UP)
+
+    # gera os níveis
+    n = 1
+    while n != num_levels+1:
+
+        # gerar a sequência de cores aleatória
+        colours = ["red", "green", "blue"]
+        random_sequence = []
+        for _ in range(n):
+            random_sequence.append(choice(colours))
+        # mostrar a sequência de cores gerada piscando os LEDs por ordem
+        for colour in random_sequence:
+            if colour == "red":
+                for _ in range(2):
+                    red.value(not red.value())
+                    sleep(pause)
+            elif colour == "green":
+                for _ in range(2):
+                    green.value(not green.value())
+                    sleep(pause)
+            elif colour == "blue":
+                for _ in range(2):
+                    blue.value(not blue.value())
+                    sleep(pause)
+
+        sleep(0.5)
+
+        # sinalizar ao jogador que pode começar a jogada
+        for _ in range(4):
+            green.value(not green.value())
+            sleep(1 / 10)
+
+        # guardar a resposta do jogador
+        moves = []
+        while 1:
+
+            if not butred.value():  # pressiona vermelho
+                moves.append("red")
+                sleep(0.3)
+
+            if not butgreen.value():  # pressiona verde
+                moves.append("green")
+                sleep(0.3)
+
+            if not butblue.value():  # pressiona azul
+                moves.append("blue")
+                sleep(0.3)
+
+            if not butreset.value():  # dar reset à resposta
+                moves = []
+                sleep(0.3)
+
+            if not butsubmit.value():  # submeter a resposta
+                break
+
+        sleep(1)
+
+        # mostrar se a resposta está correta ou não
+        answer = (moves == random_sequence)
+        if answer:
+            for _ in range(6):
+                green.value(not green.value())
+                sleep(1 / 7)
+        else:
+            for _ in range(6):
+                red.value(not red.value())
+                sleep(1 / 7)
+            n -= 1  # repetir o nível
+
+        sleep(3)
+        # passar para o próximo nível
+        n += 1
+
+    # desligar os LEDs
+    red.value(1)
+    green.value(1)
+    blue.value(1)
+
+
+def colour_cycle(pinr=25, ping=21, pinb=22, cycles=3, pause=0.5):
+    """
+    Cria um ciclo de cores (off -> vermelho -> verde -> azul -> off) no LED RGB.
+    O LED RGB deve estar em lógica negativa.
+    No final de execução de todos os ciclos, a cor vermelha pisca três vezes.
+    --- Parâmetros ---
+    - pinr: pin ao qual está ligado a perna RED
+    - ping: pin ao qual está ligado a perna GREEN
+    - pinb: pin ao qual está ligado a perna BLUE
+    - cycles: número de ciclos
+    - pause: pausa entre ciclos
+    """
+
+    # definir os sinais PWM
+    f = 1_000_000
+
+    red = PWM(Pin(pinr))
+    red.freq(f)
+    red.duty(1023)
+
+    green = PWM(Pin(ping))
+    green.freq(f)
+    green.duty(1023)
+
+    blue = PWM(Pin(pinb))
+    blue.freq(f)
+    blue.duty(1023)
+
+    # fazer o ciclo de cores durante cycles vezes
+    cycle_num = 0
+    while 1:
+
+        # quebrar o ciclo quando fizer todas as vezes
+        if cycle_num == cycles:
+            t1 = 0
+            t2 = 1023
+            # piscar o LED vermelho para mostrar que terminou
+            for _ in range(6):
+                red.duty(t1)
+                sleep(1 / 7)
+                t1, t2 = t2, t1
+            break
+        # transição desligado -> vermelho
+        t = 1023
+        while 1:
+            if t < 0:
+                t = 0
+                break
+            red.duty(t)
+            sleep(1 / 500)
+            t -= 5
+        # transição vermelho -> verde
+        green.duty(0)
+        while 1:
+            if t > 1023:
+                t = 0
+                break
+            red.duty(t)
+            sleep(1 / 500)
+            t += 1
+        # transição verde -> azul
+        blue.duty(0)
+        while 1:
+            if t > 1023:
+                t = 0
+                break
+            green.duty(t)
+            sleep(1 / 500)
+            t += 1
+        # transição azul -> desligado
+        while 1:
+            if t > 1023:
+                t = 0
+                break
+            blue.duty(t)
+            sleep(1 / 500)
+            t += 5
+        sleep(pause)
+        cycle_num += 1
+    # desligar os LEDs, mas libertá-los para uso (desligar os sinais PWM)
+    red.deinit()
+    red = Pin(pinr, Pin.OUT)
+    red.value(1)
+
+    green.deinit()
+    green = Pin(ping, Pin.OUT)
+    green.value(1)
+
+    blue.deinit()
+    blue = Pin(pinb, Pin.OUT)
+    blue.value(1)
--- main.py
+++ main.py
@@ -1,14 +1,75 @@
-from machine import Pin, PWM, DAC, ADC
+from games import colour_cycle, memory, morse
+from machine import Pin, PWM
+from time import sleep_us, sleep
 
+# colour_cycle(cycles=2)
+memory(difficulty=2)
+# morse()
 
-# Parameters
-PWMfreq = 10_000
+#red = Pin(25, Pin.OUT)
+#green = Pin(21, Pin.OUT)
+#blue = Pin(22, Pin.OUT)
 
+#red.value(1)
+#green.value(0)
+#blue.value(0)
 
-# LED output pins in PWM
-RED = PWM(Pin(25), freq=PWMfreq, duty_u16=0)
-GRE = PWM(Pin(21), freq=PWMfreq, duty_u16=0)
-BLU = PWM(Pin(22), freq=PWMfreq, duty_u16=0)
+# f=1000000
+# red = PWM(Pin(25))
+# red.freq(f)
+# red.duty(1023)
 
+# green = PWM(Pin(21))
+# green.freq(f)
+# green.duty(1023)
 
+# blue = PWM(Pin(22))
+# blue.freq(f)
+# blue.duty(1023)
+
+# r = 0
+# g = 0
+# b = 0
+
+# NO = Pin(14, Pin.IN, Pin.PULL_UP)
+# NE = Pin(13, Pin.IN, Pin.PULL_UP)
+# SO = Pin(10, Pin.IN, Pin.PULL_UP)
+# SE = Pin(9, Pin.IN, Pin.PULL_UP)
+# C = Pin(12, Pin.IN, Pin.PULL_UP)
+
+# while True:
+
+#     if not SO.value(): # roxo
+#         g = 1023
+#         r = b = 0
+
+#     if not C.value(): # branco
+#         g = r = b = 0
+
+#     if not SE.value(): # vermelho
+#         g = b = 1023
+#         r = 0
+
+#     if not NO.value(): # verde
+#         g = 0
+#         r = b = 1023
+
+#     if not NE.value(): # azul
+#         b = 0
+#         r = g = 1023
+
+#     red.duty(r)
+#     sleep_us(1)
+#     red.duty(1023)
+#     sleep_us(1)
+
+#     green.duty(g)
+#     sleep_us(1)
+#     green.duty(1023)
+#     sleep_us(1)
+
+#     blue.duty(b)
+#     sleep_us(1)
+#     blue.duty(1023)
+#     sleep_us(1)
 
